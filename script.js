f763d3ea279519a6577b15989cc9b8277c1d5ac8
Mohammad Suliman joya <sjoya66@gmail.com>
2022-11-22 17:29:07 +0430

    Add javascript for moble menu

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+document.querySelectorAll(".menu-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+  })
+);
